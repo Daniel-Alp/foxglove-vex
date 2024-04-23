3358c340f12001856c7f605574603e2d8e9c26c7
Daniel Alp <daniel.alp.email@gmail.com>
2024-04-22 23:07:46 -0400

    Convert serial bytes to string

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import serial
 ser = serial.Serial('/dev/ttyACM1', baudrate=115200, timeout=1)
 
 while True:
-    data = ser.readline()
-    print(data)
+    data = ser.readline()[6:].decode('utf-8')
+    print(data, end='')
