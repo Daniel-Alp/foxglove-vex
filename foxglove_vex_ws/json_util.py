35e7982c0760cc7cc98a550e9bb68aab7d379bde
Daniel Alp <daniel.alp.email@gmail.com>
2024-05-26 00:33:43 -0400

    infer schema based on json provided

--- foxglove_vex_ws/json_util.py
+++ foxglove_vex_ws/json_util.py
@@ -2,23 +2,29 @@ import orjson
 
 def build_shema(json):
     json_type = type(json)
-
-    #Currently does not support arrays
     if json_type is dict:
+        schema = {}
+        schema["type"] = "object"
+        schema["properties"] = {}
         for key in json:
-            build_shema(json[key])        
+            schema["properties"][key] = build_shema(json[key])
+        return schema
+
     elif json_type is str:
-        print("string", json)
+        return {"type": "string"}   
+    
     elif json_type is float or json_type is int:
-        print("number", json)
+        return {"type": "number"}
+    
     elif json_type is bool:
-        print("boolean", json)
+        return {"type": "boolean"}
 
 if __name__ == "__main__":
     example = "{\"message\": \"hello, world!\",\"nested object\": {\"x\": 123,\"y\": 4.56}, \"indicator\": true}"
     json = orjson.loads(example) 
+    schema = build_shema(json)
 
     print()
     print(orjson.dumps(json, option=orjson.OPT_INDENT_2).decode("utf-8"))
     print()
-    build_shema(json)
+    print(orjson.dumps(schema, option=orjson.OPT_INDENT_2).decode("utf-8"))
