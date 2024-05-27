681dfe7baa46a70adc2d65c2bcc43f95a04ffebc
Daniel Alp <daniel.alp.email@gmail.com>
2024-05-27 17:58:07 -0400

    move serial functions into class

--- foxglove_vex_ws/serial_util.py
+++ foxglove_vex_ws/serial_util.py
@@ -2,19 +2,21 @@ import asyncio
 import serial
 from cobs import cobs
 
-def find_vex_port():
-    pass 
+class VexSerial:
+    def __init__(self):
+        # In the future, needs to automatically find the port that the device is connected to
+        self.ser = serial.Serial('/dev/ttyACM1', baudrate=115200, timeout=0.01)
 
-async def read(ser: serial.Serial) -> str:
-    raw_data = bytearray()
-    while True:
-        next = ser.read(1)
-        if next == b'\0':
-            break
-        else:
-            raw_data.extend(next)
-        await asyncio.sleep(0)
-    return cobs.decode(raw_data).decode("utf-8")[4:]
+    async def read_packet(self) -> str:
+        raw_data = bytearray()
+        while True:
+            next = self.ser.read(1)
+            if next == b'\0':
+                break
+            else:
+                raw_data.extend(next)
+            await asyncio.sleep(0)
+        return cobs.decode(raw_data).decode("utf-8")[4:]
 
 if __name__ == "__main__":
     pass
--- foxglove_vex_ws/server.py
+++ foxglove_vex_ws/server.py
@@ -5,19 +5,19 @@ import serial
 from foxglove_websocket import run_cancellable
 from foxglove_websocket.server import FoxgloveServer
 from json_util import build_shema
-from serial_util import read
+from serial_util import VexSerial
 
 async def main():
     async with FoxgloveServer("0.0.0.0", 8765, "foxglove-vex-bridge") as server:
         logger = logging.getLogger("FoxgloveServer")
-        ser = serial.Serial('/dev/ttyACM1', baudrate=115200, timeout=0.01) # In the future, needs to automatically find the port that the device is connected to
+        ser = VexSerial()
         topic_dict = {}
 
         while True:
             timestamp = time.time_ns()
 
             try: 
-                data = await read(ser)
+                data = await ser.read_packet()
             except serial.serialutil.SerialException:
                 logger.error("Device disconnected")
                 break
