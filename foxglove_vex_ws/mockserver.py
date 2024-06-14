7c2ca68a5b0109216504df06bbd6beb1ab37ff50
Daniel Alp <daniel.alp.email@gmail.com>
2024-06-14 12:11:36 -0400

    make mock server advertise two channels

--- foxglove_vex_ws/mockserver.py
+++ foxglove_vex_ws/mockserver.py
@@ -18,9 +18,18 @@ async def main():
     }
 
     async with FoxgloveServer("0.0.0.0", 8765, "foxglove-vex-bridge") as server:
-        chan_id = await server.add_channel(
+        chan_id_1 = await server.add_channel(
             {
-                "topic": "example_trajectory",
+                "topic": "small_circle",
+                "encoding": "json",
+                "schemaName": "odometry",
+                "schema": orjson.dumps(schema).decode("utf-8"),
+                "schemaEncoding": "jsonschema"
+            }
+        )
+        chan_id_2 = await server.add_channel(
+            {
+                "topic": "big_circle",
                 "encoding": "json",
                 "schemaName": "odometry",
                 "schema": orjson.dumps(schema).decode("utf-8"),
@@ -41,14 +50,21 @@ async def main():
             theta = (360 - i) * math.pi / 180;
             i = (i + 1) % 360
 
-            payload = {
+            payload_1 = {
                 "x": x,
                 "y": y,
                 "theta": theta
             }
 
-            await server.send_message(chan_id, timestamp, orjson.dumps(payload))
-            await asyncio.sleep(0.01)
+            payload_2 = {
+                "x": 2 * x,
+                "y": 2 * y,
+                "theta": theta
+            }
+
+            await server.send_message(chan_id_1, timestamp, orjson.dumps(payload_1))
+            await server.send_message(chan_id_2, timestamp, orjson.dumps(payload_2))
+            await asyncio.sleep(0)
 
 if __name__ == "__main__":
     run_cancellable(main())
