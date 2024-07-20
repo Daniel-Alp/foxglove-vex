4870bd0fabd1e2f464b667d8fdfb97a68022dff3
Daniel Alp <daniel.alp.email@gmail.com>
2024-07-19 23:02:46 -0400

    simple test of wireless reading
    If your computer is connected to your robot's controller (instead of the brain) there is only one port called VCOM.
    To communicate with the robot you must send commands following the VEX serial protocol.

--- foxglove_vex_ws/vex_serial.py
+++ foxglove_vex_ws/vex_serial.py
@@ -26,23 +26,33 @@ class DirectConnection(BaseConnection):
         return cobs.decode(data)        
 
 def create_connection() -> BaseConnection:
+    # TODO instead of specifying port find it automatically
     pass
 
-# TODO create WirelessConnection subclsas
-
 if __name__ == "__main__":
-    # ports = serial.tools.list_ports.comports()
-    # for port in ports:
-    #     print(port)
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        print(port)
 
-    ser = serial.Serial(port="/dev/ttyACM1",
+    ser = serial.Serial(port="/dev/ttyACM0",
                         baudrate=115200,
                         bytesize=serial.EIGHTBITS,
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1)
+    
+    # command to request to read from stdio (hardcoded because I only need this command)
+    # 
+    # 0xc9, 0x36, 0xb8, 0x47 header for device-bound message
+    # 0x56                   command ID
+    # 0x27                   extended command ID
+    # 0x02                   payload size
+    # 0x01                   channel 
+    # 0x00                   write length (0x56 0x27 is also used as a write command)
+    # 0xc4, 0xad             CRC-16-CCITT checksum
+    read_command = bytes([0xc9, 0x36, 0xb8, 0x47, 0x56, 0x27, 0x02, 0x01, 0x00, 0xc4, 0xad])
 
-    connection = DirectConnection(ser)
+    ser.write(read_command)
     while True:
-        data = connection.read()
-        print(data.decode("utf-8"))
+        next = ser.read()
+        print(next)
