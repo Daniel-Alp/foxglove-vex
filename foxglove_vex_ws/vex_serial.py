535efcb8f7b222ce0110c71da8f8e4e74f40bece
Daniel Alp <daniel.alp.email@gmail.com>
2024-07-21 22:04:06 -0400

    define read method for WirelessConnection class

--- foxglove_vex_ws/vex_serial.py
+++ foxglove_vex_ws/vex_serial.py
@@ -1,10 +1,11 @@
 from abc import ABCMeta, abstractmethod
+from collections import deque
 from cobs import cobs
+import time
 import serial
 import serial.tools
 import serial.tools.list_ports
 
-import time
 
 class BaseConnection(object, metaclass=ABCMeta):
     def __init__(self, ser: serial.Serial):
@@ -22,17 +23,60 @@ class DirectConnection(BaseConnection):
             next = self.ser.read()
             if next == b'\x00':
                 break
-            data.extend(next)
-        return cobs.decode(data)        
+            data.append(next)
+        try:
+            return cobs.decode(data)[4:]        
+        except cobs.DecodeError:
+            return bytes()
+
+class WirelessConnection(BaseConnection):
+    def __init__(self, ser: serial.Serial):
+        super().__init__(ser)
+        
+        change_channel_request = bytes([0xc9, 0x36, 0xb8, 0x47, 0x56, 0x10, 0x02, 0x01, 0x01, 0xa9, 0x48])
+        ser.write(change_channel_request)
+        time.sleep(2)
+        ser.read_all()
+
+        self.all_data = deque()
+
+    def read(self) -> bytes:
+        data = bytearray()
+        read_request = bytes([0xc9, 0x36, 0xb8, 0x47, 0x56, 0x27, 0x02, 0x01, 0x00, 0xc4, 0xad])
+        while True:
+            while len(self.all_data) > 0:
+                next = self.all_data.popleft()
+                if next == 0x00:
+                    try:
+                        return cobs.decode(data)[4:]
+                    except cobs.DecodeError:
+                        return bytes()
+                else:
+                    data.append(next)
+
+            ser.write(read_request)
+
+            metadata = ser.read(3) # Header bytes and ID byte
+            if metadata != bytes([0xaa, 0x55, 0x56]):
+                ser.read_all()
+                continue
+
+            size = int.from_bytes(ser.read(), 'little') 
+            if size & 0x80 == 0x80:
+                size &= 0x7f
+                size <<= 7
+                size += int.from_bytes(ser.read(), 'little')
+        
+            payload = ser.read(size) # Extended ID byte, acknowledge byte, channel byte, new data bytes, checksum bytes
+            if payload[0] != 0x27 or payload[1] != 0x76:
+                ser.read_all()
+                continue
+
+            self.all_data.extend(payload[3:-2])
 
 def create_connection() -> BaseConnection:
     # TODO instead of specifying port find it automatically
     pass
-
-
-class WirelessConnection(BaseConnection):
-    def read(self) -> bytes:                       
-        return bytes()
 
 if __name__ == "__main__":
     ports = serial.tools.list_ports.comports()
@@ -46,55 +90,7 @@ if __name__ == "__main__":
                         stopbits=serial.STOPBITS_ONE,
                         timeout=1)
     
-    transfer_channel_request = bytes([0xc9, 0x36, 0xb8, 0x47, 0x56, 0x10, 0x02, 0x01, 0x01, 0xa9, 0x48])
-    ser.write(transfer_channel_request)
-    time.sleep(2)
-    response = ser.read_all()
-    print(response)
+    connection = WirelessConnection(ser)
 
-    all_data = bytearray()
-
-    read_request = bytes([0xc9, 0x36, 0xb8, 0x47, 0x56, 0x27, 0x02, 0x01, 0x00, 0xc4, 0xad])        
-    try:
-        while True:
-            delimeter_index = all_data.find(0x00)
-            if delimeter_index != -1:
-                try:
-                    print(cobs.decode(all_data[:delimeter_index]).decode("utf-8"))
-                except cobs.DecodeError:
-                    print("failed to decode")
-                all_data = all_data[delimeter_index:]
-                all_data.pop(0)
-                continue
-
-            ser.write(read_request)
-
-            header = ser.read(2)
-
-            if header != bytes([0xaa, 0x55]):
-                ser.read_all()
-                continue
-
-            id = ser.read()
-
-            if id != bytes([0x56]):
-                ser.read_all()
-                continue
-
-            size = int.from_bytes(ser.read(), 'little') 
-            if size & 0x80 == 0x80:
-                size &= 0x7f
-                size <<= 7
-                size += int.from_bytes(ser.read(), 'little')
-        
-            payload = ser.read(size)
-
-            extended_id = payload[0]
-            acknowledge = payload[1]
-            data = payload[3:-2]
-
-            all_data.extend(data)
-
-    except KeyboardInterrupt:
-        time.sleep(1)
-        ser.read_all()
+    while True:
+        print(connection.read().decode("utf-8"))
