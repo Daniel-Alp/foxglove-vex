a41e0721f5feff265edbb0cf5f73d6211b1e127d
Daniel Alp <daniel.alp.email@gmail.com>
2024-07-26 19:27:57 -0400

    automatically detect port instead of manually specifying it

--- foxglove_vex_ws/vex_serial.py
+++ foxglove_vex_ws/vex_serial.py
@@ -6,7 +6,6 @@ import serial
 import serial.tools
 import serial.tools.list_ports
 
-
 class BaseConnection(object, metaclass=ABCMeta):
     def __init__(self, ser: serial.Serial):
         self.ser = ser
@@ -23,7 +22,7 @@ class DirectConnection(BaseConnection):
             next = self.ser.read()
             if next == b'\x00':
                 break
-            data.append(next)
+            data.extend(next)
         try:
             return cobs.decode(data)[4:]        
         except cobs.DecodeError:
@@ -34,9 +33,9 @@ class WirelessConnection(BaseConnection):
         super().__init__(ser)
         
         change_channel_request = bytes([0xc9, 0x36, 0xb8, 0x47, 0x56, 0x10, 0x02, 0x01, 0x01, 0xa9, 0x48])
-        ser.write(change_channel_request)
+        self.ser.write(change_channel_request)
         time.sleep(2)
-        ser.read_all()
+        self.ser.read_all()
 
         self.all_data = deque()
 
@@ -51,7 +50,7 @@ class WirelessConnection(BaseConnection):
                         return cobs.decode(data)[4:]
                     except cobs.DecodeError:
                         return bytes()
-                else:
+                elif next:
                     data.append(next)
 
             self.ser.write(read_request)
@@ -61,7 +60,7 @@ class WirelessConnection(BaseConnection):
                 self.ser.read_all()
                 continue
 
-            size = int.from_bytes(self.ser.read(), 'little') 
+            size = int.from_bytes(self.ser.read(), 'little') # Size sent in either 2 or 1 bytes
             if size & 0x80 == 0x80:
                 size &= 0x7f
                 size <<= 7
@@ -75,16 +74,40 @@ class WirelessConnection(BaseConnection):
             self.all_data.extend(payload[3:-2])
 
 def create_connection() -> BaseConnection:
-   pass
+    ports = serial.tools.list_ports.comports()
+    ports = [p for p in ports if p.vid == 0x2888] # VEX vendor ID
 
-if __name__ == "__main__":
-    ser = serial.Serial("/dev/ttyACM0",
+    # A controller will show up as one port.
+    controller_ports = [p for p in ports if p.location.endswith("1")]
+
+    # A brain will show up as two ports, the user and communication port.
+    # User port is for reading from the robot's stdio.
+    brain_user_ports = [p for p in ports if p.location.endswith("2")]
+
+    ports = controller_ports + brain_user_ports
+
+    if len(ports) != 1:
+        raise serial.SerialException
+
+    port = ports[0]
+    direct_connection = port.location.endswith("2")
+
+    ser = serial.Serial(port=port.device,
                         baudrate=115200,
                         bytesize=serial.EIGHTBITS,
                         parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE,
-                        timeout=1)    
+                        timeout=1)
+    
+    if direct_connection:
+        return DirectConnection(ser)
+    else:
+        return WirelessConnection(ser)
 
-    connection = WirelessConnection(ser)
-    while True:
-        print(connection.read().decode("utf-8"))
+if __name__ == "__main__":
+    try:
+        connection = create_connection()
+        while True:
+            print(connection.read().decode("utf-8"))
+    except serial.SerialException:
+        print("Failed to connect. Zero or multiple devices connected.")
